0c49cbbb3f23fbecb6035cf550250c6d56491e81
Tiago Mello <me@rustfun.dev>
2023-12-10 21:49:13 +0100

    Use clap for command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 use say::say;
 
+use clap::{Arg, Command};
+
 fn main() {
-    say("Hello, world!");
+    hello();
+}
+
+fn hello() {
+    let matches = Command::new("hello")
+        .version("1.0")
+        .author("Tiago Mello")
+        .about("A simple hello world program")
+        .arg(
+            Arg::new("name")
+                .short('n')
+                .long("name")
+                .help("The name to greet"),
+        )
+        .get_matches();
+
+    let message = {
+        let name = matches.get_one::<String>("name").unwrap();
+        format!("Hello, {name}!")
+    };
+
+    say(&message);
 }
